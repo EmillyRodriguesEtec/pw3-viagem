f6cfea7ca8dcbd35096e1df4575d6edd975f6707
Emyrick <Emyrick@test>
2023-09-05 11:07:03 -0300

    0509

--- src/main/java/br/com/etechoracio/viagem/controller/ViagemController.java
+++ src/main/java/br/com/etechoracio/viagem/controller/ViagemController.java
@@ -4,10 +4,12 @@ import br.com.etechoracio.viagem.entity.Viagem;
 import br.com.etechoracio.viagem.repository.ViagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/viagens")
@@ -18,5 +20,10 @@ public class ViagemController {
     @GetMapping
     public List<Viagem> buscarTodos(){
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Viagem> buscarPorId(@PathVariable Long id){
+        return repository.findById(id);
     }
 }
