429becbe4c3cfecd0d21bfe88a4936a2e5c24823
EmyRick <EmyRick@test>
2023-09-12 11:04:09 -0300

    Métodos put e post

--- src/main/java/br/com/etechoracio/viagem/controller/ViagemController.java
+++ src/main/java/br/com/etechoracio/viagem/controller/ViagemController.java
@@ -3,10 +3,7 @@ package br.com.etechoracio.viagem.controller;
 import br.com.etechoracio.viagem.entity.Viagem;
 import br.com.etechoracio.viagem.repository.ViagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,4 +23,23 @@ public class ViagemController {
     public Optional<Viagem> buscarPorId(@PathVariable Long id){
         return repository.findById(id);
     }
+
+    @PostMapping //insere o body no banco de dados
+    public Viagem inserir(@RequestBody Viagem body){ //body é um objeto do tipo viagem
+        Viagem inserida = repository.save(body); //o save devolve o objeto do tipo viagem (body)
+        return inserida;
+    }
+
+    @PutMapping("/{id}")
+    public Viagem alterar(@RequestBody Long id,
+                          @PathVariable Viagem obj){
+
+        Optional<Viagem> existe = buscarPorId(id);
+        if(existe.isPresent())
+            repository.save(obj);
+        return obj;
+    }
+
+
 }
+
